0785c25e995af13996a910c19faf9bea24f78bad
Sergey Nikitin <snik2003@list.ru>
2018-08-13 11:23:35 +0300

    Initial Commit

---
+++ MetasoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetasoTest
+//
+//  Created by Сергей Никитин on 13.08.2018.
+//  Copyright © 2018 Sergey Nikitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
